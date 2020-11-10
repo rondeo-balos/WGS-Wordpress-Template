7870d404f1dddee09a968906069ade1dccba05ca
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2020-11-11 01:09:27 +0800

    fixed unexpected "<" on line 7
    Parse error: syntax error, unexpected '<' in C:\xampp\htdocs\test\wp-content\themes\WGS-Wordpress-Template-main\page.php on line 7

--- page.php
+++ page.php
@@ -4,8 +4,10 @@ get_header();
 if(have_posts()){
   while(have_posts){
     the_post();
+?>
     <h2><a href="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
     <p><?php the_content(); ?></p>
+<?php
   }
 }else
   echo "The page you are looking is not found!";
