40c684ee9c125bbf5fa0fce55711fb30d65b4c29
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2020-11-10 20:35:10 +0800

    added header

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <title><?php bloginfo('name'); ?></title>
+</head>
+<body <?php body_class(); ?>>
+  
+  <!-- header
+    bloginfo('name');
+    bloginfo('description');
+  -->
