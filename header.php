93638dd7f0f45845b5186a7d902d205088470331
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2020-11-10 22:55:27 +0800

    fixing some wrong syntaxes

--- header.php
+++ header.php
@@ -2,13 +2,14 @@
 <html <?php language_attributes(); ?>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <title><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></title>
+  <title>"><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  
-  <!-- header
-    bloginfo('name');
-    bloginfo('description');
-  -->
+  <!-- header -->
+  <a href="<?php echo home_url(); ?>"><h2><?php bloginfo('name'); ?></h2></a>
+  <?php bloginfo('description'); ?>
+  <?php $args = ['theme_location'=>'primary'];?>
+  <?php wp_nav_menu(); ?>
+  <!-- end of header -->
