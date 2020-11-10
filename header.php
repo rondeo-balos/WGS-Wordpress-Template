031fa3f08e35e612285277db983b446d9634f469
Rondeo Balos <47505842+rondeo-balos@users.noreply.github.com>
2020-11-11 01:33:02 +0800

    fixed the title tags

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <title>"><?php bloginfo('name'); ?></title>
+  <title><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <?php wp_head(); ?>
 </head>
